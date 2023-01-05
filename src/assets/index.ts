629c02ff8fbecfd28f269ead10fb8ab26fb34eac
M Abrar Khalid <abrarprince471@gmail.com>
2023-01-05 18:29:09 +0500

    services section done

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -2,10 +2,12 @@ import PngAboutusimg from './aboutusimg.png'
 import PngHeaderlogo from './headerlogo.png'
 import PngHomesectionback from './homesectionback.png'
 import SvgReact from './react.svg'
+import PngServicesback from './servicesback.png'
 
 export {
   PngAboutusimg,
   PngHeaderlogo,
   PngHomesectionback,
-  SvgReact
+  SvgReact,
+  PngServicesback
 }
--- src/screens/index.jsx
+++ src/screens/index.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { PngServicesback } from "../assets";
 import { aboutus } from "./aboutus";
 import { Homesection } from "./Homesection";
 
@@ -7,6 +8,79 @@ export default function index() {
     <>
       {Homesection()}
       {aboutus()}
+      <section className="services">
+        <div className="services__content__heading">Our Services</div>
+        <div className="services__content">
+          <div className="services__content__left">
+            <div className="services__content__left__content">
+              <div className="services__content__left__content__heading">
+                Fund Raising- Equity + Bank Financing
+              </div>
+              <div className="services__content__left__content__heading">
+                Merger and Acquisition Advisory{" "}
+              </div>
+              <div className="services__content__left__content__heading">
+                Corporate Finance Advisory{" "}
+              </div>
+              <div className="services__content__left__content__heading">
+                Finance & Tax Services{" "}
+              </div>
+            </div>
+          </div>
+
+          <div className="services__content__right">
+            <div className="services__content__right__content">
+              <div className="services__content__right__content__heading">
+                Reverse Merger – Technical Listing
+              </div>
+              <div className="services__content__right__content__span"></div>
+              <div className="services__content__right__content__text">
+                Identification of Company for acquisition Complete understanding
+                of substantial takeover laws Coordination with CCP, MTO, SECP,
+                PSX Tax & legal structuring of the transactions
+              </div>
+            </div>
+            <div className="services__content__right__content">
+              <div className="services__content__right__content__heading">
+                Reverse Merger – Technical Listing
+              </div>
+              <div className="services__content__right__content__span"></div>
+              <div className="services__content__right__content__text">
+                Identification of Company for acquisition Complete understanding
+                of substantial takeover laws Coordination with CCP, MTO, SECP,
+                PSX Tax & legal structuring of the transactions
+              </div>
+            </div>
+          </div>
+          <div className="services__content__right">
+            <div className="services__content__right__content">
+              <div className="services__content__right__content__heading">
+                Reverse Merger – Technical Listing
+              </div>
+              <div className="services__content__right__content__span"></div>
+              <div className="services__content__right__content__text">
+                Identification of Company for acquisition Complete understanding
+                of substantial takeover laws Coordination with CCP, MTO, SECP,
+                PSX Tax & legal structuring of the transactions
+              </div>
+            </div>
+            <div className="services__content__right__content">
+              <div className="services__content__right__content__heading">
+                Reverse Merger – Technical Listing
+              </div>
+              <div className="services__content__right__content__span"></div>
+              <div className="services__content__right__content__text">
+                Identification of Company for acquisition Complete understanding
+                of substantial takeover laws Coordination with CCP, MTO, SECP,
+                PSX Tax & legal structuring of the transactions
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="services__background__img">
+          <img src={PngServicesback} alt="PngServicesback" />
+        </div>
+      </section>
     </>
   );
 }
