7aa5fa3aefdf4494510ea2fccbbb15f7265b4bea
M Abrar Khalid <abrarprince471@gmail.com>
2023-01-06 19:44:17 +0500

    clients swiper done

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,6 +1,14 @@
 import PngAboutusimg from './aboutusimg.png'
 import PngAboutusimg1 from './aboutusimg1.png'
 import PngAboutusimg2 from './aboutusimg2.png'
+import PngClientswiperimg from './clientswiperimg.png'
+import PngClientswiperimg1 from './clientswiperimg1.png'
+import PngClientswiperimg2 from './clientswiperimg2.png'
+import PngClientswiperimg3 from './clientswiperimg3.png'
+import PngClientswiperimg4 from './clientswiperimg4.png'
+import PngClientswiperimg5 from './clientswiperimg5.png'
+import PngClientswiperimg6 from './clientswiperimg6.png'
+import PngClientswiperimg7 from './clientswiperimg7.png'
 import PngHeaderlogo from './headerlogo.png'
 import PngHomesectionback from './homesectionback.png'
 import PngIndustriesleftimg from './industriesleftimg.png'
@@ -14,6 +22,14 @@ export {
   PngAboutusimg,
   PngAboutusimg1,
   PngAboutusimg2,
+  PngClientswiperimg,
+  PngClientswiperimg1,
+  PngClientswiperimg2,
+  PngClientswiperimg3,
+  PngClientswiperimg4,
+  PngClientswiperimg5,
+  PngClientswiperimg6,
+  PngClientswiperimg7,
   PngHeaderlogo,
   PngHomesectionback,
   PngIndustriesleftimg,
--- src/screens/index.jsx
+++ src/screens/index.jsx
@@ -1,5 +1,13 @@
 import React from "react";
 import {
+  PngClientswiperimg,
+  PngClientswiperimg1,
+  PngClientswiperimg2,
+  PngClientswiperimg3,
+  PngClientswiperimg4,
+  PngClientswiperimg5,
+  PngClientswiperimg6,
+  PngClientswiperimg7,
   PngIndustriesleftimg,
   PngMamberbackground,
   PngServicesback,
@@ -1122,6 +1130,133 @@ export default function index() {
             </SwiperSlide>
           </Swiper>
         </div>
+      </section>
+      <section className="clients">
+        <div className="clients__heading">Our clients</div>
+        <div className="clients__content">
+          <Swiper
+            className="swiper-container"
+            // install Swiper modules
+
+            spaceBetween={50}
+            slidesPerView={6}
+            onSwiper={(swiper) => console.log(swiper)}
+            onSlideChange={() => console.log("slide change")}
+          >
+            <SwiperSlide>
+              <div className="client__card">
+                <div className="client__card__content">
+                  <img src={PngClientswiperimg} alt="PngClientswiperimg" />
+                </div>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="client__card">
+                <div className="client__card__content">
+                  <img src={PngClientswiperimg1} alt="PngClientswiperimg" />
+                </div>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="client__card">
+                <div className="client__card__content">
+                  <img src={PngClientswiperimg2} alt="PngClientswiperimg" />
+                </div>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="client__card">
+                <div className="client__card__content">
+                  <img src={PngClientswiperimg3} alt="PngClientswiperimg" />
+                </div>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="client__card">
+                <div className="client__card__content">
+                  <img src={PngClientswiperimg4} alt="PngClientswiperimg" />
+                </div>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="client__card">
+                <div className="client__card__content">
+                  <img src={PngClientswiperimg5} alt="PngClientswiperimg" />
+                </div>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="client__card">
+                <div className="client__card__content">
+                  <img src={PngClientswiperimg6} alt="PngClientswiperimg" />
+                </div>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="client__card">
+                <div className="client__card__content">
+                  <img src={PngClientswiperimg7} alt="PngClientswiperimg" />
+                </div>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="client__card">
+                <div className="client__card__content">
+                  <img src={PngClientswiperimg} alt="PngClientswiperimg" />
+                </div>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="client__card">
+                <div className="client__card__content">
+                  <img src={PngClientswiperimg1} alt="PngClientswiperimg" />
+                </div>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="client__card">
+                <div className="client__card__content">
+                  <img src={PngClientswiperimg2} alt="PngClientswiperimg" />
+                </div>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="client__card">
+                <div className="client__card__content">
+                  <img src={PngClientswiperimg3} alt="PngClientswiperimg" />
+                </div>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="client__card">
+                <div className="client__card__content">
+                  <img src={PngClientswiperimg4} alt="PngClientswiperimg" />
+                </div>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="client__card">
+                <div className="client__card__content">
+                  <img src={PngClientswiperimg5} alt="PngClientswiperimg" />
+                </div>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="client__card">
+                <div className="client__card__content">
+                  <img src={PngClientswiperimg6} alt="PngClientswiperimg" />
+                </div>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="client__card">
+                <div className="client__card__content">
+                  <img src={PngClientswiperimg7} alt="PngClientswiperimg" />
+                </div>
+              </div>
+            </SwiperSlide>
+          </Swiper>
+        </div>{" "}
       </section>
     </>
   );
